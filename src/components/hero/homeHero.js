38a81933ab35f4fa011646573d0d7c01b8af3521
Kevin T Reynolds <Kevin.T.Reynolds30@gmail.com>
2019-03-03 12:45:24 -0700

    change hero image query params

--- src/components/hero/homeHero.js
+++ src/components/hero/homeHero.js
@@ -74,7 +74,7 @@ const HomeHero = () => (
             title
             image {
               childImageSharp {
-                fluid(maxWidth: 2048, quality: 100) {
+                fluid(maxWidth: 720) {
                   ...GatsbyImageSharpFluid
                 }
               }
