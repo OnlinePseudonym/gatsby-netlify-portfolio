5b360ed494cdbd7ed52eb3f4338abe878b0116fe
Kevin T Reynolds <Kevin.T.Reynolds30@gmail.com>
2019-03-03 17:41:58 -0700

    add color widgets for homepage blobs

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from 'netlify-cms';
 
-import * as ColorWidget from 'netlify-cms-widget-color';
+import ColorWidget from 'netlify-cms-widget-color';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
